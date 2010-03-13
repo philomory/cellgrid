35bd9ae3bf87fb1871f2fd646752a19e18c0e109
philomory <philomory@localhost>
2010-03-12 20:25:46 -1000

    Gemspec tweaking.

--- gemspec.rb
+++ gemspec.rb
@@ -4,4 +4,7 @@ spec = Gem::Specification.new do |s|
   s.summary = "Grid system for use in games."
   s.description = "A robust grid and cell structure for use in games with discrete maps, such as rogue-likes."
   s.files = Dir['lib/**/*.rb'] + Dir['test/**/*.rb'] + ['Rakefile.rb']
+  s.author = "Adam Gardner"
+  s.email = "adam.oddfellow@gmail.com"
+  s.homepage = "http://bitbucket.org/philomory/cellgrid/"
 end
